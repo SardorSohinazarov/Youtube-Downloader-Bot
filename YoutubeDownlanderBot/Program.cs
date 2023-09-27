2ea2ca56045a27e8cf4384a82413468ac7ea2258
user <user@WIN-0GEV2J052PH>
2023-09-27 14:32:54 +0500

    Add project files.

---
+++ YoutubeDownlanderBot/Program.cs
@@ -0,0 +1,87 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VideoLibrary;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var botClient = new TelegramBotClient("{Your bot token}");
+
+        using CancellationTokenSource cts = new();
+
+        // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        var me = await botClient.GetMeAsync();
+
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+
+        // Send cancellation request to stop bot
+        cts.Cancel();
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Only process Message updates: https://core.telegram.org/bots/api#message
+            if (update.Message is not { } message)
+                return;
+            // Only process text messages
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            //Downland
+            if (messageText.Contains("https://youtu.be/") || messageText.Contains("https://www.youtube.com"))
+            {
+                YouTube youTube = YouTube.Default;
+                YouTubeVideo youTubeVideo = youTube.GetVideo(messageText);
+                var stream = youTubeVideo.GetBytes();
+                Stream stream2 = new MemoryStream(stream);
+
+                await botClient.SendVideoAsync(
+                    chatId: chatId,
+                    InputFile.FromStream(stream: stream2),
+                    cancellationToken: cancellationToken);
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId:chatId,
+                    text: "You tube link tashang deyapman o'yin qilyapsimi tushunmadm",
+                    replyToMessageId:message.MessageId,
+                    cancellationToken: cancellationToken
+                    );
+            }
+        }
+
+        Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
